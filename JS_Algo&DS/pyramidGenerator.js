8df7f6fd37a6e5517de3d58f05980ebc43b451ca
prajwal-dev <prajwaldilipbhagat@gmail.com>
2025-06-12 23:18:50 +0530

    Updated code

--- JS_Algo&DS/pyramidGenerator.js
+++ JS_Algo&DS/pyramidGenerator.js
@@ -1,4 +1,11 @@
-// 
+// Topics Covered
+// -let,const 
+// for () for of
+// functions,arguments, parameters
+// scope,block scope, global scope
+// falsy & truthy value 
+
+
 
 const character = "#";
 const count = 8;
@@ -7,7 +14,7 @@ for (let i = 0; i < count; i = i + 1) {
     rows.push(character.repeat(i + 1))
   }
   
-  let result = ""
+  let result = "";
   
   for (const row of rows) {
     result = result + row + "\n";
@@ -15,3 +22,15 @@ for (let i = 0; i < count; i = i + 1) {
   
   console.log(result);
 //   Till Step 46
+
+function padRow(name) {
+    return name;
+  }
+
+function addTwoNumbers(num1, num2) {
+    return num1 + num2;
+  }
+  
+  const sum = addTwoNumbers(5, 10);
+  console.log(sum)
+  // till step 56
